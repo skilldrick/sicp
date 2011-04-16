ecb4bacd7c5911dfabfea9227f9d8dc0f1f8e0fa
Skilldrick <skilldrick@gmail.com>
2011-04-17 00:22:02 +0100

    Completed ex3.07

---
+++ ex3.07.scm
@@ -0,0 +1,40 @@
+(define (make-account balance password)
+  (define tries 0)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (bad-password amount)
+    (set! tries (+ tries 1))
+    (if (> tries 7)
+        (call-the-cops)
+        "Incorrect password"))
+  (define (dispatch m p)
+    (if (eq? password p)
+        (cond ((eq? m 'withdraw) withdraw)
+              ((eq? m 'deposit) deposit)
+              (else (error "Unknown request -- MAKE-ACCOUNT"
+                           m)))
+        bad-password))
+  dispatch)
+
+(define (make-joint acct old-pass new-pass)
+  (define (dispatch m p)
+    (if (eq? new-pass p)
+        (acct m old-pass)
+        (lambda (amount)
+          "Incorrect password")))
+  dispatch)
+
+(define acct (make-account 100 'blah))
+
+((acct 'withdraw 'blah) 50)
+
+(define bob (make-joint acct 'blah 'baa))
+
+((bob 'withdraw 'baa) 10)
+
