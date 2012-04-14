813639deb539d716c15450eb065a0a9e87c5d906
Nick Morgan <skilldrick@gmail.com>
2012-04-13 23:57:59 -0700

    Got it working! (except for lisp-value)

--- qeval.scm
+++ qeval.scm
@@ -44,6 +44,12 @@
       (apply stream-map
              (cons proc (map stream-cdr argstreams))))))
 
+(define (stream-append s1 s2)
+  (if (stream-null? s1)
+    s2
+    (cons-stream (stream-car s1)
+                 (stream-append (stream-cdr s1) s2))))
+
 (define (show x)
   (display-line x)
   x)
@@ -80,7 +86,20 @@
 
 
 
+(define *op-table* (make-hash))
 
+(define (put op type proc)
+  (hash-set! *op-table* (list op type) proc))
+
+(define (get op type)
+  (hash-ref *op-table* (list op type) #f))
+
+
+
+(define input-prompt ";;; Query input:")
+(define output-prompt ";;; Query results:")
+(define (prompt-for-input string)
+  (newline) (newline) (display string) (newline))
 
 (define (query-driver-loop)
   (prompt-for-input input-prompt)
@@ -159,25 +178,6 @@
 
 (put 'not 'qeval negate)
 
-(define (lisp-value call frame-stream)
-  (stream-flatmap
-    (lambda (frame)
-      (if (execute
-            (instantiate
-              call
-              frame
-              (lambda (v f)
-                (error "Unknown pat var - LISP-VALUE" v))))
-        (singleton-stream frame)
-        the-empty-stream))
-    frame-stream))
-
-(put 'lisp-value 'qeval lisp-value)
-
-(define (execute exp)
-  (apply (eval (predicate exp) user-initial-environment)
-         (args exp)))
-
 (define (always-true ignore frame-stream) frame-stream)
 
 (put 'always-true 'qeval always-true)
@@ -185,7 +185,7 @@
 (define (find-assertions pattern frame)
   (stream-flatmap
     (lambda (datum)
-      (check-an-assert datum pattern frame))
+      (check-an-assertion datum pattern frame))
     (fetch-assertions pattern frame)))
 
 (define (check-an-assertion assertion query-pat query-frame)
@@ -211,4 +211,351 @@
     (if binding
       (pattern-match (binding-value binding) dat frame)
       (extend var dat frame))))
+
+(define (apply-rules pattern frame)
+  (stream-flatmap (lambda (rule)
+                    (apply-a-rule rule pattern frame))
+                  (fetch-rules pattern frame)))
+
+(define (apply-a-rule rule query-pattern query-frame)
+  (let ((clean-rule (rename-variables-in rule)))
+    (let ((unify-result
+            (unify-match query-pattern
+                         (conclusion clean-rule)
+                         query-frame)))
+      (if (eq? unify-result 'failed)
+        the-empty-stream
+        (qeval (rule-body clean-rule)
+               (singleton-stream unify-result))))))
+
+(define (rename-variables-in rule)
+  (let ((rule-application-id (new-rule-application-id)))
+    (define (tree-walk exp)
+      (cond ((var? exp)
+             (make-new-variable exp rule-application-id))
+            ((pair? exp)
+             (cons (tree-walk (car exp))
+                   (tree-walk (cdr exp))))
+            (else exp)))
+    (tree-walk rule)))
+
+(define (unify-match p1 p2 frame)
+  (cond ((eq? frame 'failed) 'failed)
+        ((equal? p1 p2) frame)
+        ((var? p1) (extend-if-possible p1 p2 frame))
+        ((var? p2) (extend-if-possible p2 p1 frame))
+        ((and (pair? p1) (pair? p2))
+         (unify-match (cdr p1)
+                      (cdr p2)
+                      (unify-match (car p1)
+                                   (car p2)
+                                   frame)))
+        (else 'failed)))
+
+(define (extend-if-possible var val frame)
+  (let ((binding (binding-in-frame var frame)))
+    (cond (binding
+            (unify-match
+              (binding-value binding) val frame))
+          ((var? val)
+           (let ((binding (binding-in-frame val frame)))
+             (if binding
+               (unify-match
+                 var (binding-value binding) frame)
+               (extend var val frame))))
+          ((depends-on? val var frame)
+           'failed)
+          (else (extend var val frame)))))
+
+(define (depends-on? exp var frame)
+  (define (tree-walk e)
+    (cond ((var? e)
+           (if (equal? var e)
+             true
+             (let ((b (binding-in-frame e frame)))
+               (if b
+                 (tree-walk (binding-value b))
+                 false))))
+          ((pair? e)
+           (or (tree-walk (car e))
+               (tree-walk (cdr e))))
+          (else false)))
+  (tree-walk exp))
+
+(define THE-ASSERTIONS the-empty-stream)
+
+(define (fetch-assertions pattern frame)
+  (if (use-index? pattern)
+    (get-indexed-assertions pattern)
+    (get-all-assertions)))
+
+(define (get-all-assertions) THE-ASSERTIONS)
+
+(define (get-indexed-assertions pattern)
+  (get-stream (index-key-of pattern) 'assertion-stream))
+
+(define (get-stream key1 key2)
+  (let ((s (get key1 key2)))
+    (if s s the-empty-stream)))
+
+(define THE-RULES the-empty-stream)
+
+(define (fetch-rules pattern frame)
+  (if (use-index? pattern)
+    (get-indexed-rules pattern)
+    (get-all-rules)))
+
+(define (get-all-rules) THE-RULES)
+
+(define (get-indexed-rules pattern)
+  (stream-append
+    (get-stream (index-key-of pattern) 'rule-stream)
+    (get-stream '? 'rule-stream)))
+
+(define (add-rule-or-assertion! assertion)
+  (if (rule? assertion)
+    (add-rule! assertion)
+    (add-assertion! assertion)))
+
+(define (add-assertion! assertion)
+  (store-assertion-in-index assertion)
+  (let ((old-assertions THE-ASSERTIONS))
+    (set! THE-ASSERTIONS
+      (cons-stream assertion old-assertions))
+    'ok))
+
+(define (add-rule! rule)
+  (store-rule-in-index rule)
+  (let ((old-rules THE-RULES))
+    (set! THE-RULES (cons-stream rule old-rules))
+    'ok))
+
+(define (store-assertion-in-index assertion)
+  (if (indexable? assertion)
+    (let ((key (index-key-of assertion)))
+      (let ((current-assertion-stream
+              (get-stream key 'assertion-stream)))
+        (put key
+             'assertion-stream
+             (cons-stream assertion
+                          current-assertion-stream))))
+    '()))
+
+(define (store-rule-in-index rule)
+  (let ((pattern (conclusion rule)))
+    (if (indexable? pattern)
+      (let ((key (index-key-of pattern)))
+        (let ((current-rule-stream
+                (get-stream key 'rule-stream)))
+          (put key
+               'rule-stream
+               (cons-stream rule
+                            current-rule-stream))))
+      '())))
+
+(define (indexable? pat)
+  (or (constant-symbol? (car pat))
+      (var? (car pat))))
+
+(define (index-key-of pat)
+  (let ((key (car pat)))
+    (if (var? key) '? key)))
+
+(define (use-index? pat)
+  (constant-symbol? (car pat)))
+
+(define (stream-append-delayed s1 delayed-s2)
+  (if (stream-null? s1)
+    (force delayed-s2)
+    (cons-stream
+      (stream-car s1)
+      (stream-append-delayed (stream-cdr s1) delayed-s2))))
+
+(define (interleave-delayed s1 delayed-s2)
+  (if (stream-null? s1)
+    (force delayed-s2)
+    (cons-stream
+      (stream-car s1)
+      (interleave-delayed (force delayed-s2)
+                          (delay (stream-cdr s1))))))
+
+(define (stream-flatmap proc s)
+  (flatten-stream (stream-map proc s)))
+
+(define (flatten-stream stream)
+  (if (stream-null? stream)
+    the-empty-stream
+    (interleave-delayed
+      (stream-car stream)
+      (delay (flatten-stream (stream-cdr stream))))))
+
+(define (singleton-stream x)
+  (cons-stream x the-empty-stream))
+
+(define (type exp)
+  (if (pair? exp)
+    (car exp)
+    (error "Unknown expression TYPE" exp)))
+
+(define (contents exp)
+  (if (pair? exp)
+    (cdr exp)
+    (error "Unknown expression CONTENTS" exp)))
+
+(define (assertion-to-be-added? exp)
+  (eq? (type exp) 'assert!))
+
+(define (add-assertion-body exp)
+  (car (contents exp)))
+
+(define (empty-conjunction? exps) (null? exps))
+(define (first-conjunct exps) (car exps))
+(define (rest-conjuncts exps) (cdr exps))
+
+(define (empty-disjunction? exps) (null? exps))
+(define (first-disjunct exps) (car exps))
+(define (rest-disjuncts exps) (cdr exps))
+
+(define (negated-query exps) (car exps))
+
+(define (predicate exps) (car exps))
+(define (args exps) (cdr exps))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+    (eq? (car exp) tag)
+    #f))
+
+(define (rule? statement)
+  (tagged-list? statement 'rule))
+
+(define (conclusion rule) (cadr rule))
+
+(define (rule-body rule)
+  (if (null? (cddr rule))
+    '(always-true)
+    (caddr rule)))
+
+(define (query-syntax-process exp)
+  (map-over-symbols expand-question-mark exp))
+
+(define (map-over-symbols proc exp)
+  (cond ((pair? exp)
+         (cons (map-over-symbols proc (car exp))
+               (map-over-symbols proc (cdr exp))))
+        ((symbol? exp) (proc exp))
+        (else exp)))
+
+(define (expand-question-mark symbol)
+  (let ((chars (symbol->string symbol)))
+    (if (string=? (substring chars 0 1) "?")
+      (list '?
+            (string->symbol
+              (substring chars 1 (string-length chars))))
+      symbol)))
+
+(define (var? exp)
+  (tagged-list? exp '?))
+
+(define (constant-symbol? exp) (symbol? exp))
+
+(define rule-counter 0)
+
+(define (new-rule-application-id)
+  (set! rule-counter (+ 1 rule-counter))
+  rule-counter)
+
+(define (make-new-variable var rule-application-id)
+  (cons '? (cons rule-application-id (cdr var))))
+
+(define (contract-question-mark variable)
+  (string->symbol
+    (string-append "?"
+                   (if (number? (cadr variable))
+                     (string-append (symbol->string (caddr variable))
+                                    "-"
+                                    (number->string (cadr variable)))
+                     (symbol->string (cadr variable))))))
+
+(define (make-binding variable value)
+  (cons variable value))
+
+(define (binding-variable binding)
+  (car binding))
+
+(define (binding-value binding)
+  (cdr binding))
+
+(define (binding-in-frame variable frame)
+  (assoc variable frame))
+
+(define (extend variable value frame)
+  (cons (make-binding variable value) frame))
+
+(define (setup-database)
+  (define (add-assertions! assertions)
+    (for-each add-rule-or-assertion! (map query-syntax-process assertions)))
+  (add-assertions!
+    '(
+      (address (Bitdiddle Ben) (Slumerville (Ridge Road) 10))
+      (job (Bitdiddle Ben) (computer wizard))
+      (salary (Bitdiddle Ben) 60000)
+
+      (address (Hacker Alyssa P) (Cambridge (Mass Ave) 78))
+      (job (Hacker Alyssa P) (computer programmer))
+      (salary (Hacker Alyssa P) 40000)
+      (supervisor (Hacker Alyssa P) (Bitdiddle Ben))
+
+      (address (Fect Cy D) (Cambridge (Ames Street) 3))
+      (job (Fect Cy D) (computer programmer))
+      (salary (Fect Cy D) 35000)
+      (supervisor (Fect Cy D) (Bitdiddle Ben))
+
+      (address (Tweakit Lem E) (Boston (Bay State Road) 22))
+      (job (Tweakit Lem E) (computer technician))
+      (salary (Tweakit Lem E) 25000)
+      (supervisor (Tweakit Lem E) (Bitdiddle Ben))
+
+      (address (Reasoner Louis) (Slumerville (Pine Tree Road) 80))
+      (job (Reasoner Louis) (computer programmer trainee))
+      (salary (Reasoner Louis) 30000)
+      (supervisor (Reasoner Louis) (Hacker Alyssa P))
+
+      (supervisor (Bitdiddle Ben) (Warbucks Oliver))
+      (address (Warbucks Oliver) (Swellesley (Top Heap Road)))
+      (job (Warbucks Oliver) (administration big wheel))
+      (salary (Warbucks Oliver) 150000)
+
+      (address (Scrooge Eben) (Weston (Shady Lane) 10))
+      (job (Scrooge Eben) (accounting chief accountant))
+      (salary (Scrooge Eben) 75000)
+      (supervisor (Scrooge Eben) (Warbucks Oliver))
+
+      (address (Cratchet Robert) (Allston (N Harvard Street) 16))
+      (job (Cratchet Robert) (accounting scrivener))
+      (salary (Cratchet Robert) 18000)
+      (supervisor (Cratchet Robert) (Scrooge Eben))
+
+      (address (Aull DeWitt) (Slumerville (Onion Square) 5))
+      (job (Aull DeWitt) (administration secretary))
+      (salary (Aull DeWitt) 25000)
+      (supervisor (Aull DeWitt) (Warbucks Oliver))
+
+      (can-do-job (computer wizard) (computer programmer))
+      (can-do-job (computer wizard) (computer technician))
+
+      (can-do-job (computer programmer)
+                  (computer programmer trainee))
+
+      (can-do-job (administration secretary)
+                  (administration big wheel))
+      (rule (lives-near ?person-1 ?person-2)
+            (and (address ?person-1 (?town . ?rest-1))
+                 (address ?person-2 (?town . ?rest-2))
+                 (not (same ?person-1 ?person-2))))
+      (rule (same ?x ?x))
+     )))
+
+(setup-database)
+(query-driver-loop)
 
