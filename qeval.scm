d1cd2ba4d4518bf05e100b6fcfc3d93a2704cb17
Nick Morgan <skilldrick@gmail.com>
2012-04-15 22:29:28 -0700

    Exercise 4.63

--- qeval.scm
+++ qeval.scm
@@ -584,6 +584,24 @@
       (rule (last-pair (?el) (?el)))
       (rule (last-pair (?x . ?rest) ?wat)
             (last-pair ?rest ?wat))
+
+      (son Adam Cain)
+      (son Cain Enoch)
+      (son Enoch Irad)
+      (son Irad Mehujael)
+      (son Mehujael Methushael)
+      (son Methushael Lamech)
+      (wife Lamech Ada)
+      (son Ada Jabal)
+      (son Ada Jubal)
+
+      (rule (grandson ?grandfather ?grandson)
+            (and (son ?grandfather ?father)
+                 (son ?father ?grandson)))
+      (rule (son ?father ?son)
+            (and (wife ?father ?mother)
+                 (son ?mother ?son)))
+
      )))
 
 (setup-database)
