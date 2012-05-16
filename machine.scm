5418746941c9df549071b3fabde3651bb1f543a1
Nick Morgan <skilldrick@gmail.com>
2012-05-15 21:59:48 -0700

    Done ex5.14

--- machine.scm
+++ machine.scm
@@ -35,22 +35,37 @@
   ((register 'set) value))
 
 (define (make-stack)
-  (let ((s '()))
+  (let ((s '())
+        (number-pushes 0)
+        (max-depth 0)
+        (current-depth 0))
     (define (push x)
-      (set! s (cons x s)))
+      (set! s (cons x s))
+      (set! number-pushes (+ 1 number-pushes))
+      (set! current-depth (+ 1 current-depth))
+      (set! max-depth (max current-depth max-depth)))
     (define (pop)
       (if (null? s)
         (error "Empty stack -- POP")
         (let ((top (car s)))
           (set! s (cdr s))
+          (set! current-depth (- current-depth 1))
           top)))
     (define (initialize)
       (set! s '())
+      (set! number-pushes 0)
+      (set! max-depth 0)
+      (set! current-depth 0)
       'done)
+    (define (print-statistics)
+      (newline)
+      (display (list 'total-pushes '= number-pushes
+                     'maximum-depth '= max-depth)))
     (define (dispatch message)
       (cond ((eq? message 'push) push)
             ((eq? message 'pop) (pop))
             ((eq? message 'initialize) (initialize))
+            ((eq? message 'print-statistics) (print-statistics))
             (else (error "Unknown request -- STACK"
                          message))))
     dispatch))
@@ -81,7 +96,9 @@
         (the-instruction-sequence '()))
     (let ((the-ops
             (list (list 'initialize-stack
-                        (lambda () (stack 'initialize)))))
+                        (lambda () (stack 'initialize)))
+                  (list 'print-stack-statistics
+                        (lambda () (stack 'print-statistics)))))
           (register-table
             (list (list 'pc pc) (list 'flag flag))))
       (define (allocate-register name)
@@ -441,6 +458,47 @@
   (set-register-contents! expt-machine 'n 4)
   (start expt-machine)
   (assert-equal (get-register-contents expt-machine 'val) 81 "Expt machine broken")
+
+  (define fact-machine
+    (make-machine
+      '(continue n val)
+      (list (list '* *) (list '- -) (list '= =))
+      '((perform (op initialize-stack))
+        (assign continue (label fact-done))
+        fact-loop
+        (test (op =) (reg n) (const 1))
+        (branch (label base-case))
+        (save continue)
+        (save n)
+        (assign n (op -) (reg n) (const 1))
+        (assign continue (label after-fact))
+        (goto (label fact-loop))
+        after-fact
+        (restore n)
+        (restore continue)
+        (assign val (op *) (reg n) (reg val))
+        (goto (reg continue))
+        base-case
+        (assign val (const 1))
+        (goto (reg continue))
+        fact-done
+        (perform (op print-stack-statistics)))))
+
+  (set-register-contents! fact-machine 'n 5)
+  (start fact-machine)
+  (assert-equal (get-register-contents fact-machine 'val) 120 "Fact machine broken")
+
+  (define (loop n)
+    (if (< n 10)
+      (begin
+        (newline)
+        (newline)
+        (display (list 'n n))
+        (set-register-contents! fact-machine 'n n)
+        (start fact-machine)
+        (loop (+ n 1)))))
+  (loop 2)
+
   )
 
 (test-make-machine)
